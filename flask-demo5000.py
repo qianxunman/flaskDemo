f1c823153bba230cb27298dbbe35c785c5087fd4
luojun <qianxunman1@outlook.com>
2019-03-11 00:04:48 +0800

    tencent version1

--- flask-demo5000.py
+++ flask-demo5000.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return 'hello world, this is runing in port 5000!'
+    return 'hello world, this is runing in port 5000!</br><h1>不用看了，你就是一个大傻子！哈哈哈哈</h1>'
 
 if __name__ == '__main__':
     app.run(debug=True)
